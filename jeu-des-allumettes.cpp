6bc4d5320cdb852ace7231e6410245488d414bba
Sherlock Thomes <thomaswiscart@icloud.com>
2021-11-04 17:42:21 +0100

    Amélioration de la procédure saisie() à l'aide du contrôle de saisie et d'autres améliorations dans le programme
    Les 3 grandes améliorations sont : le changement de nom des variables (je l'expliquerais en cours en présentiel), la mise en place des contrôles de flux et la correction de certaines erreurs orthographiques.

--- jeu-des-allumettes.cpp
+++ jeu-des-allumettes.cpp
@@ -4,34 +4,47 @@
 using namespace std;
 
 // Définition de la procédure saisie (procédure à faire dans la sous-tâche 1)
-void saisie(string *nom, char *niv, int *nb_allumettes, string *prem) {
+void saisie(string *nomUtilisateur, char *niveauUtilisateur, int *nombre_allumettes, string *premierJoueur) {
   // Demande le nom du joueur
   cout<<"Quel est ton nom de joueur?"<<endl;
-  cin>>*nom;
+  cin>>*nomUtilisateur;
 
-  // Demande à l'utilisateur quel est le niveau qu'il veut pour l'ordinateur
-  cout<<"Tu vas jouer contre l'ordinateur. Quel niveau souhaites-tu qu'il ai?"<<endl;
-  cout<<"Tapes n ou N si tu souhaites mettre le niveau sur Naif"<<endl;
-  cout<<"Tapes e ou E si tu souhaites mettre le niveau sur Expert"<<endl;
-  cin>>*niv;
+  // Début du contrôle de saisie
+  do {
+      // Demande à l'utilisateur quel est le niveau qu'il veut pour l'ordinateur
+      cout<<"Tu vas jouer contre l'ordinateur. Quel niveau souhaites-tu qu'il ait?"<<endl;
+      cout<<"Tapes n ou N si tu souhaites mettre le niveau sur Naif"<<endl;
+      cout<<"Tapes e ou E si tu souhaites mettre le niveau sur Expert"<<endl;
+      cin>>*niveauUtilisateur;
+  // Condition qui contrôle la saisie
+  } while (*niveauUtilisateur != 'e' and *niveauUtilisateur != 'E' and *niveauUtilisateur != 'n' and *niveauUtilisateur != 'N');
 
-  // Demande à l'utilisateur quel est le nombre d'allumettes qu'il souhaite
-  cout<<"Quel est le nombre d'allumettes que tu souhaites (entre 0 et 30 allumettes)?"<<endl;
-  cin>>*nb_allumettes;
 
-  // Demande à l'utilisateur quel est le premier joueur entre lui et l'ordinateur
-  cout<<"Choississez le premier joueur :"<<endl;
-  cout<<"Si vous voulez laisser l'ordinateur jouer en premier, tapez Ordinateur"<<endl;
-  cout<<"Si vous voulez être le premier joueur, écrivez votre nom : "<<*nom<<endl;
-  cin>>*prem;
+  // Début du contrôle de saisie
+  do {
+      // Demande à l'utilisateur quel est le nombre d'allumettes qu'il souhaite
+      cout<<"Quel est le nombre d'allumettes que tu souhaites (entre 0 et 30 allumettes)?"<<endl;
+      cin>>*nombre_allumettes;
+  // Condition qui contrôle la saisie
+  } while (*nombre_allumettes < 0 or *nombre_allumettes > 30);
+
+// Début du contrôle de saisie
+  do {
+      // Demande à l'utilisateur quel est le premier joueur entre lui et l'ordinateur
+      cout<<"Choississez le premier joueur :"<<endl;
+      cout<<"Si vous voulez laisser l'ordinateur jouer en premier, tapez Ordinateur"<<endl;
+      cout<<"Si vous voulez être le premier joueur, écrivez votre nom : "<<*nomUtilisateur<<endl;
+      cin>>*premierJoueur;
+  // Condition qui contrôle la saisie
+  } while (*premierJoueur != "Ordinateur" and *premierJoueur != *nomUtilisateur);
 }
 int main() {
   // Déclaration des variables
   // Le nombre d'allumettes est un entier
   int nb_allumettes;
-  // Le niveau est donné à l'aide uniquement un caractère
+  // Le niveau est donné à l'aide uniquement d'un caractère
   char niv;
-  // Le nom et le premier joueur sont des ensembles de caractères
+  // Le nom et le premier joueur sont des chaines de caractères
   string nom, prem;
 
   // Appel de la procédure Saisie (procédure à faire dans la sous-tâche 1)
